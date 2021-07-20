8783007a19d84b24a03ffe733effeeb1f6c195bb
Tom K <tomknighton@icloud.com>
2021-07-20 13:07:56 +0100

    fix(chat): Proper timezones for message dates

--- components/chat/ChatMessage.tsx
+++ components/chat/ChatMessage.tsx
@@ -25,30 +25,48 @@ export default function ChatMessageView({
       isSameSender &&
       !isBotOrAdminMessage &&
       !isReply &&
-      Math.floor((Math.abs(new Date(chatMessage.messageCreatedAt).getTime() - new Date(previousChatMessage.messageCreatedAt).getTime())/100)/60) <= 7
+      Math.floor(
+        Math.abs(
+          new Date(chatMessage.messageCreatedAt).getTime() -
+            new Date(previousChatMessage.messageCreatedAt).getTime()
+        ) /
+          100 /
+          60
+      ) <= 7
     );
   }
 
   const isWithin = isWithinPreviousMessage();
-
+  const adjustForUTCOffset = date => {
+    return new Date(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate(),
+      date.getUTCHours(),
+      date.getUTCMinutes(),
+      date.getUTCSeconds(),
+    );
+  };
   return (
-    <div className={`flex flex-row w-full ${isWithin ? '': 'mt-3'} hover:bg-gray-100`}>
-      <div className={`mr-4 ${isWithin ? 'hidden' : 'block'}`}>
+    <div
+      className={`flex flex-row w-full ${
+        isWithin ? "" : "mt-3"
+      } hover:bg-gray-100`}
+    >
+      <div className={`mr-4 ${isWithin ? "hidden" : "block"}`}>
         <img
           src={chatMessage.userDTO.userProfileImageUrl}
           className="w-12 h-12 rounded-full shadow"
         />
       </div>
-      <div className={`flex-1 flex flex-col ${isWithin ? 'ml-16' : ''}`}>
-        <h3 className={`font-bold ${isWithin ? 'hidden' : 'block'}`}>
-          {chatMessage.userDTO.userFullName} at{" "}
-          {DateTime.fromMillis(
-            new Date(chatMessage.messageCreatedAt).getTime()
-          ).toRelative()}
+      <div className={`flex-1 flex flex-col ${isWithin ? "ml-16" : ""}`}>
+        <h3 className={`font-bold ${isWithin ? "hidden" : "block"}`}>
+          {chatMessage.userDTO.userFullName} {" "}
+          <span className="font-extralight">{DateTime.fromSQL(chatMessage.messageCreatedAt).toRelative({
+            base: DateTime.fromJSDate(adjustForUTCOffset(new Date())),
+          })}</span>
         </h3>
-        <p>
-          {chatMessage.messageContent}
-        </p>
+        <p>{chatMessage.messageContent}</p>
       </div>
     </div>
   );
